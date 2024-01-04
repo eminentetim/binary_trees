18bdb541c5660ea33c46912c8074655ef1705cc8
etimeminent@gmail.com <etimeminent@gmail.com>
2024-01-04 06:51:06 -0800

    updating

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,15 +12,14 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+
+	new_node->n = value;
 
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-
-	/*Assign data to this node*/
-	new_node->n = value;
 	new_node->parent = parent;
 	new_node->left = NULL;
 	new_node->right = NULL;
